b620e6ba178b04d3257b722a4d353e7d4211070b
Ravi kumar <ravi.kc12@gmail.com>
2017-10-18 00:21:25 +0530

    minor indentation changes

--- BooleanString/BooleanString.java
+++ BooleanString/BooleanString.java
@@ -7,15 +7,13 @@
             scanner.nextLine();
             for(int c = 0; c < testCases; c++) {
                 String input = scanner.nextLine();
-                LinkedList<Character> queue = new LinkedList<Character>();
-                
+                LinkedList<Character> queue = new LinkedList<Character>();                
                 
                 for(char ch : input.toCharArray()) queue.add(ch);
                 int result = Character.getNumericValue(queue.removeFirst());
                 while(!queue.isEmpty()) {                
                     Character operator = queue.removeFirst();
-                    int operand = Character.getNumericValue(queue.removeFirst());
-                    System.out.println("operands: " + result + " " + operand + " operator: " + operator);
+                    int operand = Character.getNumericValue(queue.removeFirst());                    
                     switch(operator) {
                         case 'A': result = result & operand;
                             break;
