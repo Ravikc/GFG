acb994a1f23b75c5423aec9cb6ef6e6854bc946e
Ravi kumar <ravi.kc12@gmail.com>
2017-10-20 15:54:03 +0530

    removed redundant code

--- CarryCounter/CarryCounter.java
+++ CarryCounter/CarryCounter.java
@@ -1,21 +1,5 @@
 import java.util.*;
 public class CarryCounter {
-	private static void makeEqualInLength(String str1, String str2) {
-		int len1 = str1.length();
-		int len2 = str2.length();
-		
-		if(len1 < len2) {
-			for(int i = 0; i < len2 - len1; i++) {
-				str1 = '0' + str1;
-			}
-		}
-		else if(len2 < len1) {
-			for(int i = 0; i < len1 - len2; i++) {
-				str2 = '0' + str2;
-			}
-		}
-	}
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int T = Integer.parseInt(scanner.nextLine());
