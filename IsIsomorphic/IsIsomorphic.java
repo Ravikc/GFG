341c0abc369785c05a03581cdbecccb5762d8995
Ravi kumar <ravi.kc12@gmail.com>
2017-11-03 21:53:08 +0530

    fixed typo

--- IsIsomorphic/IsIsomorphic.java
+++ IsIsomorphic/IsIsomorphic.java
@@ -1,5 +1,14 @@
 import java.util.*;
 public class IsIsomorphic {
+	public boolean isIsomorphic(Node root1, Node root2) {
+		if(root1 == null && root2 == null) return true;
+		if((root1 == null || root2 == null) || root1.data != root2.data) return false;
+		
+		return isIsomorphic(root1.left, root2.left) && isIsomorphic(root1.right, root2.right) ||
+				isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right, root2.left);
+   	}
+
 	public static void main(String[] args) {
+		//auto-generated code
 	} 
 }
